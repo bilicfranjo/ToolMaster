08bf77b8de5ca42ccac7680e4d5c1ff9756c9ee5
bilicfranjo <bilic.franjo5@gmail.com>
2025-05-06 17:56:17 +0200

    Make first version of category

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subcategories')
+    image = models.ImageField(upload_to='categories/', null=True, blank=True)
     
     def __str__(self):
         return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Banner
+from .models import Banner, Category
 
 def home_view(request):
     banners = Banner.objects.all()
-    return render(request, 'shop/home.html', {'banners' : banners,})
+    categories = Category.objects.filter(image__isnull=False)[:4]
+    return render(request, 'shop/home.html', {'banners' : banners, 'categories': categories})
 
 
